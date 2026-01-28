a6a762b41b61f27a26c1cc0ee2fda54b50af186a
Dre Santos <dre.santoz@gmail.com>
2026-01-27 21:04:17 -0300

    Add book content

--- web/js/rulebook/constants.js
+++ web/js/rulebook/constants.js
@@ -5,14 +5,15 @@
 export const LAST_CHAPTER_KEY = "maeriLastChapter";
 export const LAST_TOPIC_KEY = "maeriLastTopic";
 
-/*
- * Lista oficial de capítulos do Rulebook
- */
+
+//Lista oficial de capítulos do Rulebook
+
 
 // Chapters list
 export const RULEBOOK_CHAPTERS = [
-  { id:"01", file: "01-o-basico.json", title: "O Básico" },
+  { id:"01", file: "01-fundamentos.json", title: "Fundamentos" },
   { id:"02", file: "02-personagem.json", title: "Personagem" },
-  { id:"03", file: "03-combate.json", title: "Combate" }
+  { id:"03", file: "03-combate.json", title: "Combate" },
+  { id:"04", file: "04-magia.json", title: "Magia"}
   // Depois você adiciona os próximos aqui:
 ];
