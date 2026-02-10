3142eca2438113dbd499ba779adcb8c548058291
Dre Santos <dre.santoz@gmail.com>
2026-02-10 03:29:57 -0300

    Small fixes on maeri chapter

--- web/js/rulebook/constants.js
+++ web/js/rulebook/constants.js
@@ -17,4 +17,7 @@ export const RULEBOOK_CHAPTERS = [
   { id:"04", file: "04-magia.json", title: "Magia" },
   { id:"05", file: "05-circulo-social-comercio.json", title: "Circulo Social e Comercio" },
   { id:"06", file: "06-seres.json", title: "Seres" },
+  { id:"07", file: "07-classes.json", title: "Classes" },
+  { id:"08", file: "08-maeri.json", title: "Maeri" },
+  { id:"09", file: "09-campanha.json", title: "Campanha" },
 ];
